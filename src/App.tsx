3642b5e3dd48ccdca36a89a2b5ee34e04f8d3f77
vama-shivantra <235020113+vama-shivantra@users.noreply.github.com>
2026-05-04 09:40:47 +0530

    fix: change contact route

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Contact from "./components/ContactForm";
 import Layout from "./components/Layout";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
